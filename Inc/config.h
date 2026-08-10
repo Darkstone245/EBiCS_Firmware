d7d9ee2e2f10ac3b0999bc2cdbfa77ffd5ceb9a9
Darkstone245 <71212843+Darkstone245@users.noreply.github.com>
2026-08-10 13:16:00 +0200

    Update config.h

--- Inc/config.h
+++ Inc/config.h
@@ -100,10 +100,10 @@
 
 //----------------------------- Display setting
 
-#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_618U
+#define DISPLAY_TYPE DISPLAY_TYPE_DEBUG
 
 //------------------------------Autodetect setting
-#define AUTODETECT 0
+#define AUTODETECT 1
 
 
 
