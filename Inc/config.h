214df9447528eea9c49211022249dc1cffe59258
Darkstone245 <71212843+Darkstone245@users.noreply.github.com>
2026-08-13 10:20:30 +0200

    Change display type and disable autodetect setting

--- Inc/config.h
+++ Inc/config.h
@@ -101,10 +101,10 @@
 
 //----------------------------- Display setting
 
-#define DISPLAY_TYPE DISPLAY_TYPE_DEBUG
+#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_618U
 
 //------------------------------Autodetect setting
-#define AUTODETECT 1
+#define AUTODETECT 0
 
 
 
